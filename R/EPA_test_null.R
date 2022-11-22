796c9617c430fe4af474f4723b2761209b7eb9e7
Dr Christis Katsouris <43221582+christiskatsouris@users.noreply.github.com>
2022-11-22 12:52:19 +0200

    Update EPA_test_null.R

--- R/EPA_test_null.R
+++ R/EPA_test_null.R
@@ -46,3 +46,32 @@ time    <- as.matrix( dataset$Date )
 
 RV  <- read.csv( "RV.csv",  header = TRUE )
 RV  <- as.matrix(RV)
+
+###############################################################################
+### Function 1: fast_LASSO_HAR_MODEL (Priority Lasso Shrinkage Norm)
+###############################################################################
+
+fast_LASSO_HAR_MODEL <- function(  x = X.corrected , y = Y1, which.firm =  which.firm  )
+{# BEGIN OF FUNCTION
+  
+  x <- x
+  y <- y
+  k <- which.firm
+  
+  # At this step, we re-arrange the design matrix so that it has always the 3 lagged RVs of the particular firm at the first 3 columns
+  x.firm   <- x[ , ( (3*k-2):(3*k) ) ]
+  x.others <- x[  , -( (3*k-2):(3*k) ) ]
+  x.new    <- cbind( x.firm , x.others  ) 
+  
+  # cv <- cv.glmnet( x = x , y = y, alpha=1,nfolds=10 )
+  # l  <- cv$lambda.min
+  # alpha <- 1
+  
+  blocks  <- list( bp1 = 1:3, bp2 = 4:81 )
+  fits <- prioritylasso( X = x.new , Y = y, family = "gaussian", type.measure = "mse", blocks = blocks, standardize = FALSE, block1.penalization = FALSE )
+  
+  res <- as.matrix( as.vector( fits$coefficients ) )
+  return( res )
+  
+}# END OF FUNCTION
+
